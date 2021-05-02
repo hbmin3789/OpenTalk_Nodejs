66fb9afec19138e68b911b7a74dd6ca7f62c1255
hbmin3789 <hbmin3789@naver.com>
2021-05-02 17:38:13 +0900

    add ssl code to react project

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const api = require('./routes/index');
 const cors = require('cors');
 const {InitWebSocket} = require('./src/network/webSocket');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 
 const PORT = 3002;
 
@@ -13,3 +15,10 @@ app.use(cors());
 app.use('/api',api);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const options = {
+  key: fs.readFileSync(__dirname + '/cert/rootca.key'),
+  cert: fs.readFileSync(__dirname + '/cert/rootca.crt')
+};
+
+https.createServer(options, app).listen(3030);
