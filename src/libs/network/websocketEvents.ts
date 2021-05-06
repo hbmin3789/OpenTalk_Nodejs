18170580b603e3b09e1201ca4e47a2051445fafb
hbmin3789 <hbmin3789@naver.com>
2021-05-06 11:42:17 +0900

    fix error

--- src/libs/network/websocketEvents.ts
+++ src/libs/network/websocketEvents.ts
@@ -4,8 +4,7 @@ export const setSocketEvent = (evName: string, invokeMethod: (data: any) => void
     SocketEvents[evName] = invokeMethod;
 }
 
-export const WebSocketEvents = async (resp: any) => {
-    resp = JSON.parse(resp);    
+export const WebSocketEvents = async (resp: any) => {   
     try{
         SocketEvents[resp.message](resp);
     } catch(e) {
